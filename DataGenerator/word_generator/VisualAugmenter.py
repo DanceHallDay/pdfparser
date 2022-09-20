fd477222f9c3cc1f9eff91f91f0f09260753f2d5
Nikita Aparovich <o.pelmen.o@gmail.com>
2022-09-20 15:40:12 +0300

    add x_starts and x_ends to augmentation

--- DataGenerator/word_generator/VisualAugmenter.py
+++ DataGenerator/word_generator/VisualAugmenter.py
@@ -1,9 +1,7 @@
 from .IVisualAugmenter import IVisualAugmenter
-from typing import Any, List, Tuple
+from typing import List, Tuple
 import numpy as np
 import cv2
-import imutils
-
 
 
 class VisualAugmenterRotation(IVisualAugmenter):
@@ -33,22 +31,19 @@ class VisualAugmenterRotation(IVisualAugmenter):
     def augment(
             self, 
             img: np.array, 
-            *args, 
-            **kwrgs
-        ) -> np.array | Tuple[np.array, np.array, np.array]:
+            starts_x: np.array, 
+            ends_x: np.array
+        ) -> Tuple[np.array, np.array, np.array]:
         angle = np.random.randint(self.lower, self.upper)
-        print(angle)
         (h, w) = img.shape[:2]
         center = (w // 2, h // 2)
         img = cv2.bitwise_not(img)
         M = cv2.getRotationMatrix2D(center, angle, 1.0)
         img = cv2.warpAffine(img, M, (w, h))
         img = cv2.bitwise_not(img)
-        if 'starts_x' in kwrgs and 'ends_x' in kwrgs:
-            return img, \
-                self.transform_coords(kwrgs['starts_x'], M, center[1]), \
-                self.transform_coords(kwrgs['ends_x'], M, center[1])
-        return img
+        return img, \
+            self.transform_coords(starts_x, M, center[1]), \
+            self.transform_coords(ends_x, M, center[1])
 
 
 class VisualAugmenterGaussianNoise(IVisualAugmenter):
@@ -66,10 +61,15 @@ class VisualAugmenterGaussianNoise(IVisualAugmenter):
         self.mean = mean
         self.std = std
 
-    def augment(self, img: np.array, *args, **kwrgs) -> np.array:
+    def augment(
+            self, 
+            img: np.array, 
+            starts_x: np.array, 
+            ends_x: np.array
+        ) -> Tuple[np.array, np.array, np.array]:
         gaussian = np.random.normal(self.mean, self.std, img.shape).astype(np.uint8)
         img = img + gaussian
-        return img
+        return img, starts_x, ends_x
 
 
 class VisualAugmenterStretching(IVisualAugmenter):
@@ -96,9 +96,9 @@ class VisualAugmenterStretching(IVisualAugmenter):
     def augment(
             self, 
             img: np.array, 
-            *args, 
-            **kwrgs
-        ) -> np.array | Tuple[np.array, np.array, np.array]:
+            starts_x: np.array, 
+            ends_x: np.array
+        ) -> Tuple[np.array, np.array, np.array]:
         rxl, rxu = int(self.dx[0] * 100), int(self.dx[1] * 100)
         ryl, ryu = int(self.dy[0] * 100), int(self.dy[1] * 100)
 
@@ -107,11 +107,9 @@ class VisualAugmenterStretching(IVisualAugmenter):
         width = int(img.shape[1] * strt_x)
         height = int(img.shape[0] * strt_y)
         img = cv2.resize(img, (width, height))
-        if 'starts_x' in kwrgs and 'ends_x' in kwrgs:
-            return img, \
-                self.transform_coords(kwrgs['starts_x'], strt_x), \
-                self.transform_coords(kwrgs['ends_x'], strt_x)          
-        return img
+        return img, \
+            self.transform_coords(starts_x, strt_x), \
+            self.transform_coords(ends_x, strt_x)          
 
 
 class VisualAugmenterErosion(IVisualAugmenter):
@@ -129,12 +127,17 @@ class VisualAugmenterErosion(IVisualAugmenter):
         self.iteration = iteration
         self.kernel_size_range = kernel_size_range
 
-    def augment(self, img: np.array, *args, **kwrgs) -> np.array:
+    def augment(
+            self, 
+            img: np.array,  
+            starts_x: np.array, 
+            ends_x: np.array
+        ) -> Tuple[np.array, np.array, np.array]:
         w = np.random.randint(*self.kernel_size_range)
         h = np.random.randint(*self.kernel_size_range)
         kernel = np.random.choice([0, 1], size=(w, h)).astype(np.uint8)
         img = cv2.erode(img, kernel, iterations=self.iteration)
-        return img
+        return img, starts_x, ends_x
 
 
 class VisualAugmenterDilation(IVisualAugmenter):
@@ -152,9 +155,14 @@ class VisualAugmenterDilation(IVisualAugmenter):
         self.iteration = iteration
         self.kernel_range = kernel_range
 
-    def augment(self, img: np.array, *args, **kwrgs) -> np.array:
+    def augment(
+            self, 
+            img: np.array,  
+            starts_x: np.array, 
+            ends_x: np.array
+        ) -> Tuple[np.array, np.array, np.array]:
         w = np.random.randint(*self.kernel_range)
         h = np.random.randint(*self.kernel_range)
         kernel = np.random.choice([0, 1], size=(w, h)).astype(np.uint8)
         img = cv2.dilate(img, kernel, iterations=self.iteration)
-        return img
+        return img, starts_x, ends_x
