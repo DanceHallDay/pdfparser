1e7a5836f5b1b8c12695a7533b7abd3739b2698d
Nikita Aparovich <o.pelmen.o@gmail.com>
2022-09-20 18:30:02 +0300

    update description

--- DataGenerator/word_generator/IVisualAugmenter.py
+++ DataGenerator/word_generator/IVisualAugmenter.py
@@ -6,6 +6,6 @@ import numpy as np
 class IVisualAugmenter(ABC):
     def augment(self, img: np.array, starts_x: np.array, ends_x: np.array) -> Tuple[np.array, np.array, np.array]:
         """
-        Applies augmentation technique to @img and returns an augmented image
+        Applies augmentation technique to @img, @starts_x and @ends_x and returns an augmented image and letters coordinates 
         """
         pass
