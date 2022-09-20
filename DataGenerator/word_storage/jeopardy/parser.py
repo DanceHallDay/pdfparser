5b43c07ad23171ae648235754a1b76d22c56fd59
Andrei <andrei.kot@innowise-group.com>
2022-09-20 12:57:42 +0300

    datasets

--- DataGenerator/word_storage/jeopardy/parser.py
+++ DataGenerator/word_storage/jeopardy/parser.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import itertools
+from DataGenerator.word_storage.utils import filter_uniq, save_words
+
+
+def fetch_words(row, key):
+    rows = row[key].split()
+    return rows
+
+def concat(seq):
+    res = itertools.chain(*seq)
+    return res
 
 if __name__ == '__main__':
     path_train = 'JEOPARDY_QUESTIONS1.json'
@@ -6,6 +17,16 @@ if __name__ == '__main__':
     df = pd.read_json(path_train)
     # Index(['category', 'air_date', 'question', 'value', 'answer', 'round',
     #    'show_number'],
-    print(df['question'])
-    print(df['answer'])
-    print(df['round'])
+
+    text_list_q = df['question'].to_list()
+    text_list_a = df['answer'].to_list()
+
+    text_list = text_list_q + text_list_a
+
+    text = ' '.join(text_list)
+
+    words = filter_uniq(text)
+
+    print(len(words))
+
+    save_words(words, 'jeopardy.wl')
